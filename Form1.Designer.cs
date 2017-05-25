6ff613b4414b7d25b398a9fb855f73dfa62c0164
student <student@PC-201>
2017-05-25 17:27:03 +0200

    Interfejs został zakończony

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.adres = new System.Windows.Forms.TextBox();
+            this.my_port = new System.Windows.Forms.NumericUpDown();
+            this.info_o_polaczeniach = new System.Windows.Forms.ListBox();
+            this.przycisk_start = new System.Windows.Forms.Button();
+            this.przycisk_stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.my_port)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // adres
+            // 
+            this.adres.Location = new System.Drawing.Point(13, 13);
+            this.adres.Name = "adres";
+            this.adres.Size = new System.Drawing.Size(278, 20);
+            this.adres.TabIndex = 0;
+            this.adres.Text = "Adres";
+            // 
+            // my_port
+            // 
+            this.my_port.Location = new System.Drawing.Point(342, 12);
+            this.my_port.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.my_port.Name = "my_port";
+            this.my_port.Size = new System.Drawing.Size(351, 20);
+            this.my_port.TabIndex = 1;
+            // 
+            // info_o_polaczeniach
+            // 
+            this.info_o_polaczeniach.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.info_o_polaczeniach.FormattingEnabled = true;
+            this.info_o_polaczeniach.ItemHeight = 29;
+            this.info_o_polaczeniach.Location = new System.Drawing.Point(13, 40);
+            this.info_o_polaczeniach.Name = "info_o_polaczeniach";
+            this.info_o_polaczeniach.Size = new System.Drawing.Size(680, 265);
+            this.info_o_polaczeniach.TabIndex = 2;
+            // 
+            // przycisk_start
+            // 
+            this.przycisk_start.Location = new System.Drawing.Point(77, 334);
+            this.przycisk_start.Name = "przycisk_start";
+            this.przycisk_start.Size = new System.Drawing.Size(182, 52);
+            this.przycisk_start.TabIndex = 3;
+            this.przycisk_start.Text = "Start";
+            this.przycisk_start.UseVisualStyleBackColor = true;
+            // 
+            // przycisk_stop
+            // 
+            this.przycisk_stop.Location = new System.Drawing.Point(365, 334);
+            this.przycisk_stop.Name = "przycisk_stop";
+            this.przycisk_stop.Size = new System.Drawing.Size(184, 52);
+            this.przycisk_stop.TabIndex = 4;
+            this.przycisk_stop.Text = "Stop";
+            this.przycisk_stop.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 407);
+            this.Controls.Add(this.przycisk_stop);
+            this.Controls.Add(this.przycisk_start);
+            this.Controls.Add(this.info_o_polaczeniach);
+            this.Controls.Add(this.my_port);
+            this.Controls.Add(this.adres);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.my_port)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox adres;
+        private System.Windows.Forms.NumericUpDown my_port;
+        private System.Windows.Forms.ListBox info_o_polaczeniach;
+        private System.Windows.Forms.Button przycisk_start;
+        private System.Windows.Forms.Button przycisk_stop;
     }
 }
 
